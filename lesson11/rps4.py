04c7939ccf70b4a7a789fb53593b7fb096f16a1c
Popangelov <mfaka7342@gmail.com>
2026-05-25 16:17:26 +0300

    modified

--- lesson11/rps4.py
+++ lesson11/rps4.py
@@ -2,7 +2,8 @@ import sys
 import random
 from enum import Enum
 
-gamo_count = 0
+game_count = 0
+
 
 def play_rps():
 
@@ -11,16 +12,13 @@ def play_rps():
         PAPER = 2
         SCISSORS = 3
 
-
-
-
     playerchoice = input(
         "\nEnter... \n1 for Rock,\n2 for Paper, or \n3 for Scissors:\n\n")
-    
+
     if playerchoice not in ["1", "2", "3"]:
         print("You must enter 1, 2, or 3.")
-        return play_rps
-    
+        return play_rps()
+
     player = int(playerchoice)
 
     computerchoice = random.choice("123")
@@ -28,39 +26,41 @@ def play_rps():
     computer = int(computerchoice)
 
     print("\nYou chose " + str(RPS(player)).replace('RPS.', '').title() + ".")
-    print("Python chose " + str(RPS(computer)).replace('RPS.', '').title() + ".\n")
+    print("Python chose " + str(RPS(computer)
+                                ).replace('RPS.', '').title() + ".\n")
+
     def decide_winner(player, computer):
-            
         if player == 1 and computer == 3:
-            return"🎉 You win!"
+            return "🎉 You win!"
         elif player == 2 and computer == 1:
-            return"🎉 You win!"
+            return "🎉 You win!"
         elif player == 3 and computer == 2:
-            return"🎉 You win!"
+            return "🎉 You win!"
         elif player == computer:
-            return"😲 Tie game!"
+            return "😲 Tie game!"
         else:
-            return"🐍 Python wins!"
+            return "🐍 Python wins!"
 
     game_result = decide_winner(player, computer)
-    global game_count
-    game_count += 1
 
     print(game_result)
+
+    global game_count
+    game_count += 1
 
     print("\nGame count: " + str(game_count))
 
     print("\nPlay again?")
 
     while True:
-        playagain = input("\nY for Yes or \nQ to Quit \n")
-        if playagain.lower() not in ["y" , "q"]:
+        playagain = input("\nY for Yes or \nQ to Quit\n")
+        if playagain.lower() not in ["y", "q"]:
             continue
         else:
             break
 
     if playagain.lower() == "y":
-        return play_rps
+        return play_rps()
     else:
         print("\n🎉🎉🎉🎉")
         print("Thank you for playing!\n")
@@ -68,4 +68,3 @@ def play_rps():
 
 
 play_rps()
-
